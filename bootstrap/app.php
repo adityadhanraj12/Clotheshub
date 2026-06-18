20585a55b53c1088a4dca45c79fde9e7f1123389
adityadhanraj12 <adityadhanraj404@gmail.com>
2026-06-18 17:59:21 +0530

    Trust all proxies in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/customerLogin');
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
     })
